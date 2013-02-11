6626aece3e1158577a82590658767c96ead0b928
“b.” <noise@hypefiend.com>
2013-02-11 14:05:47 -0500

    first shot functional core

---
+++ README.md
@@ -0,0 +1,129 @@
+# Leaders Overview
+
+A leaderboard service/server for online/mobile games, backed by
+Redis. Redis offers the unique ability to do sorted set intersections
+in the storage server. This allows for "friends leaderboards" to be
+computed very efficiently.
+
+# Dependencies
+
+Python 2.7.x, Redis 2.4.x+, Flask, Redis-py, RedisMock
+
+Linux:
+
+```sh
+sudo apt-get install redis-server
+virtualenv venv --distribute
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+OS X:
+
+```sh
+brew install redis
+virtualenv venv --distribute
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+# Running
+
+Tests:
+
+```sh
+./bin/runtests
+```
+
+Server:
+
+```sh
+./bin/runserver
+```
+
+# Design
+
+
+## Leaderboards
+
+Leaderboards are defined by the Game ID and Board ID. There can be any
+number of boards per game. Game ID and Board ID are both free-form
+strings, though it's best practice to keep them in the set of
+characters: "[A-Z][a-z][0-9]-_.".
+
+Leaderboards can be set to expire after N periods, see Configuration
+
+## Entries
+
+An entry on a given leaderboard consists of a User ID, and
+Value. Timestamps are added automatically. 
+
+## Values
+
+Values can either be an absoulte value, e.g. a high-score, where
+the maximum during the given period is desired, or a cumulative value,
+e.g. total coins earned in the period, number of games wons, etc.  
+
+## Time Ranges
+
+Leaderboards can be set to maintain daily, weekly, or monthly
+sets. Currently there is no support for sliding windows such as "last
+7 days".
+
+
+## Storage Design
+
+Boards keys: /leaders/{game_id}/{board_id}/{range_code}/{slot}
+* board_id can be any unique string, for example: "highscore", "earned_coins", etc.
+* range_code is one of: "d" (daily), "w" (weekly), "m" (monthly), "a" (all time)
+* slot is a range dependent format of the current date:
+  - daily: yyyymmdd
+  - weekly: yyyyww
+  - monthly: yyyymm
+  - alltime: a
+
+Board data
+* the "member" (Redis parlance) for each entry is the user_id and the "score" is the Data Point
+
+Friend lists
+* We create temp zsets for each passed in friends list, with the key based off a hash of the list.
+* You can also specify the key to an existing zset of friends (TODO)
+
+## Data Integrity
+
+The leaders system makes no attempt to verify that data passed in by
+the game is correct, aside from some basic well-formedness
+checks. Games are responsible for screening data before pushing it
+into Leaders. 
+
+Likewise, there is nothing stopping a caller from doing "set" and "inc" to the same board.
+
+# REST API
+
+The REST API is structured to match the storage design in a nearly 1:1
+manner.
+
+GETs:
+* /leaders/games
+* /leaders/{game_id}/boards
+* /leaders/{game_id}/{board_id}/{range_code}
+* /leaders/{game_id}/{board_id}/{range_code}/last
+
+POSTs:
+* URL: /leaders/{game_id}/{board_id}/{range_code}/{user_id}
+* POST vars: 
+  * "value"
+  * "type", one of: "set"(default), "inc"
+
+
+# Configuration
+
+(TODO)
+
+* expiration
+* buckets
+* ?
+
+
+
+
---
+++ leaders/leaders.py
@@ -0,0 +1,148 @@
+import datetime as dt
+import time
+import hashlib
+
+# todo - abstract storage
+from redis import Redis
+
+_KEY_DELIMITER = '/'
+
+
+class TimeRange(object):
+    def __init__(self, code, format, expiration):
+        self.range_code = code
+        self.slot_format = format
+        self.expiration = expiration
+
+    def format(self, date):
+        return _KEY_DELIMITER.join([self.range_code,
+                                   date.strftime(self.slot_format)])
+
+class Leaderboard(object):
+    """
+    Main class for leaderboards. See README.md for further documentation,
+    and tests/ directory for examples.
+    """
+
+    # Constants for specifying range(s) to Leaderboard constructor
+    _1_DAY_SECONDS = 60 * 60 * 24
+    _1_WEEK_SECONDS = _1_DAY_SECONDS * 7
+    _1_MONTH_SECONDS = _1_DAY_SECONDS * 31  # yes 30.5 on average, but extra is OK.
+    # TODO: make expiration configurable
+    RANGE_DAILY = TimeRange('d', '%Y%m%d', 3 * _1_DAY_SECONDS)
+    RANGE_WEEKLY = TimeRange('w', '%Y%w', 2 * _1_WEEK_SECONDS + 2 * _1_DAY_SECONDS)
+    RANGE_MONTHLY = TimeRange('m', '%Y%m', 2 * _1_MONTH_SECONDS + 2 * _1_DAY_SECONDS)
+    RANGE_ALLTIME = TimeRange('a', 'a', -1)
+    RANGES_ALL = [RANGE_DAILY, RANGE_WEEKLY, RANGE_MONTHLY, RANGE_ALLTIME]
+
+    def __init__(self, game, metric, ranges=RANGES_ALL):
+        self.game = game
+        self.metric = metric
+        self.ranges = ranges
+        self.store = Redis()
+
+    def _board_key(self, range, slots_ago=0):
+        """
+        Board keys are of the format:
+        /leaders/{game}/{metric}/{range_code}/{range_slot}
+        e.g. /combat/highscore/d/20130207
+        """
+        # todo: implement "slots_ago" for yesterday, last week, etc.
+        d = dt.date.fromtimestamp(time.time())
+
+        return _KEY_DELIMITER.join(["leaders", self.game, self.metric,
+                                        range.format(d)])
+
+    def _hashlist(self, l):
+        """
+        hash from a list for creating unique temp zset keys
+        """
+        h = hashlib.sha1()
+        for i in l:
+            h.update(i)
+        return h.hexdigest()
+
+    def _add_ranks(self, leaders, offset=0):
+        """
+        Our zrange calls give back user id and score/value but not rank.
+        We could call zrank but that's inefficient and currently does not deal
+        with ties.
+
+        Ranks start at 1, not 0.
+        """
+        # todo: option to decide how to handle ties
+        i = offset + 1
+        leaders2 = []
+        for l in leaders:
+            name, score = l
+            leaders2.append((name, score, i))
+            i = i + 1
+        return leaders2
+
+    def set_metric(self, user, value):
+        """
+        Set a new peak value for this user, e.g. high score
+        """
+        for r in self.ranges:
+            key = self._board_key(r)
+            self.store.zadd(key, user, value)
+            if r != self.RANGE_ALLTIME:
+                self.store.expire(key, r.expiration)
+
+    def inc_metric(self, user, value):
+        """
+        Increment the current value for this user, e.g. total earned
+        """
+        for r in self.ranges:
+            key = self._board_key(r)
+            self.store.zincrby(key, user, value)
+            if r != self.RANGE_ALLTIME:
+                self.store.expire(key, r.expiration)
+
+
+    def leaders(self, range, limit=-1, offset=0, slots_ago=0):
+        '''
+        retrieve a list of global leaders
+        '''
+        key = self._board_key(range, slots_ago)
+        end = offset + limit - 1 if limit > 0 else -1
+        l = self.store.zrange(key, offset, end, withscores=True)
+        return self._add_ranks(l, offset)
+
+    def leaders_near(self, range, user, limit, slots_ago=0):
+        '''
+        retrieve a list of global leaders surrounding the given user
+        '''
+        key = self._board_key(range, slots_ago)
+        rank = self.store.zrank(key, user)
+        print rank
+
+        l = self.store.zrange(key, max(0, rank - limit / 2 + 1),
+                              rank + limit / 2,
+                              withscores=True)
+        return self._add_ranks(l)
+
+    def leaders_friends(self, friends, range, limit=-1, offset=0, slots_ago=0):
+        '''
+        retrieve a list of leaders from the given friends list
+        '''
+        key = self._board_key(range, slots_ago)
+
+        # create a temp zset of friends to intersect w/global list
+        # todo: allow for caching the friend list via config
+        tmpid = self._hashlist(friends)
+        friends_key = 'friends_' + tmpid
+        inter_key = 'inter_' + tmpid
+        # todo: pipeline
+        for f in friends:
+            self.store.zadd(friends_key, f, 0)
+
+        self.store.zinterstore(inter_key, [key, friends_key])
+        end = offset + limit if limit > 0 else -1
+        l = self.store.zrange(inter_key, offset, end, withscores=True)
+
+        self.store.delete(friends_key)
+        self.store.delete(inter_key)
+
+        return self._add_ranks(l)
+
